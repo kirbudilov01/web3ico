73e7ccacf402e039f961fe066f109020f2c3fe54
kirbudilov01 <budilov02ke@gmail.com>
2026-01-28 23:18:56 +0300

    Updated CreatorCommentary.tsx

--- src/components/CreatorCommentary.tsx
+++ src/components/CreatorCommentary.tsx
@@ -112,7 +112,7 @@ export default function CreatorCommentary() {
               <div className="relative" style={{ paddingBottom: '56.25%' }}>
                 <iframe
                   className="absolute inset-0 w-full h-full"
-                  src="https://www.youtube.com/embed/VIDEO_ID_PRELAUNCH"
+                  src="https://www.youtube.com/embed/iMm6dSuvPc4"
                   title="Prelaunch program + token plan"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
